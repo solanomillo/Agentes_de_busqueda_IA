21285f99ec438bb2f0d3830e54dbfc4c4858a9e9
solanomillo <solanomillo144@gmail.con>
2026-06-07 16:54:58 -0300

    feat(agents): agregar módulo de agentes de búsqueda

---
+++ agents/scientific_agent.py
@@ -0,0 +1,23 @@
+"""
+Agente Científico buscando información en la web
+"""
+from agent_factory import build_agent
+from tools.arxiv_tools import buscar_arxiv
+
+CONTENT_AGENT_PROMPT ="""
+Actúa como un asistente útil y especializado en investigación.
+  -buscar_arxiv : realiza busqueda en la web y retorna los resultados.
+  cuando el usuario pregunte sobre un tema especifico:
+  1. Usa la herramienta buscar_articulos
+  2. Analiza los resultados.
+  3. Devuelve una respuesta clara.
+  4. Incluye enlaces de las fuentes utilizadas.
+"""
+
+def get_scientific_agent():
+    scientific_agent = build_agent(
+        name="ScientificAgent",
+        tools=[buscar_arxiv],
+        system_prompt=CONTENT_AGENT_PROMPT
+        )
+    return scientific_agent
---
+++ agents/web_agent.py
@@ -0,0 +1,23 @@
+"""
+Agente de Búsqueda en la web
+"""
+from agent_factory import build_agent
+from tools.tavily_tools import tavily_tool
+
+CONTENT_AGENT_PROMPT = """
+  Actúa como un asistente útil y especializado en investigación para responder la consulta del usuario.
+  -tavily_tool : realiza busqueda en la web y retorna los resultados.
+  Siempre que el usuario pregunte sobre un tema especifico:
+  1. Analiza los resultados.
+  2. Devuelve una respuesta clara.
+  3. Incluye enlaces de las fuentes utilizadas.
+"""
+
+# Construye el agente de contenido con la herramienta de búsqueda web
+def get_web_agent():
+    web_agent = build_agent(
+        name="WebAgent",
+        tools=[tavily_tool],
+        system_prompt=CONTENT_AGENT_PROMPT
+        )
+    return web_agent 
